7bd42e979716a492c155cd489ad5b9d2a7179716
rafalzzz <rzdeb94@gmail.com>
2022-12-06 19:28:51 +0100

    switch createBrowserTouter to createHashRouter

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { RickAndMortyCharacters } from "features/rick-and-morty-characters";
 import { Header } from "features/rick-and-morty-characters/components/header";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
-    path: "/rick-and-morty",
+    path: "/",
     element: <RickAndMortyCharacters />,
     errorElement: <Header title={"Page not found"} />,
   },
